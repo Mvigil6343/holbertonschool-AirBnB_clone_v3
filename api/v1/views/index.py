8440a8032f6d868c59927ab6e9e92b6ffaf97ef9
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 <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>
2024-07-07 01:27:44 -0700

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,27 @@
 """Module index from the app"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats",  strict_slashes=False)
+def count():
+    """ Count objects """
+    dictionaryOBJ = {}
+    classes = {
+        "Amenity": "amenities",
+        "Place": "places",
+        "State": "states",
+        "City": "cities",
+        "Review": "reviews",
+        "User": "users"
+    }
+
+    for cls in classes:
+        count = storage.count(cls)
+        dictionaryOBJ[classes.get(cls)] = count
+    return jsonify(dictionaryOBJ)
